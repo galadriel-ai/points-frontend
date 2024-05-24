c672727f4a6ebfc533411a6ec1223a662a291d0d
Arunavo Ray <rayanup3@gmail.com>
2024-05-24 22:58:55 +0530

    Works Now

--- app/dashboard/_components/connect-wallet.tsx
+++ app/dashboard/_components/connect-wallet.tsx
@@ -5,30 +5,10 @@ import { useEffect, useState } from "react";
 import { ConnectKitButton } from "connectkit";
 import Cookies from "js-cookie";
 import { toast } from "sonner";
-import useSWR from "swr";
 import { getAddress } from "viem";
-import { useAccount, useConnect, useDisconnect, useSignMessage } from "wagmi";
+import { useAccount, useDisconnect, useSignMessage } from "wagmi";
 
 import { Button } from "@/components/ui/button";
-
-const fetchNonceAndIssuedAt = async (url: string, walletAddress: string) => {
-  const token = Cookies.get("authToken");
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      Authorization: token || "",
-    },
-    body: JSON.stringify({ wallet_address: walletAddress }),
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
-
-  return await response.json();
-};
 
 const linkWallet = async (
   url: string,
@@ -49,87 +29,110 @@ const linkWallet = async (
     }),
   });
 
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
-
-  return await response.json();
+  return response;
 };
 
 export default function ConnectWallet() {
-  const [siweMessage, setSiweMessage] = useState<string | null>(null);
   const [linkSuccess, setLinkSuccess] = useState<boolean>(false);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
   const { address: accountAddress, isConnected } = useAccount();
-  const { connect } = useConnect();
   const { disconnect } = useDisconnect();
   const {
+    signMessage,
     data: signMessageData,
     error: signError,
-    signMessage,
   } = useSignMessage();
 
-  const {
-    data: nonceData,
-    error: nonceError,
-    mutate: fetchNonce,
-  } = useSWR(
-    accountAddress
-      ? [
+  useEffect(() => {
+    const connectWalletFlow = async () => {
+      setIsRunning(true);
+      if (isConnected && accountAddress) {
+        const token = Cookies.get("authToken");
+
+        const checksumAddress = getAddress(accountAddress);
+
+        await fetch(
           `${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/auth/eth/nonce`,
-          accountAddress,
-        ]
-      : null,
-    ([url, walletAddress]) => fetchNonceAndIssuedAt(url, walletAddress)
-  );
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+              Authorization: token || "",
+            },
+            body: JSON.stringify({ wallet_address: checksumAddress }),
+          }
+        ).then((response) => {
+          if (response.ok) {
+            response
+              .json()
+              .then((nonceData: { nonce: string; issued_at: string }) => {
+                // Set SIWE message
+                const message = `${process.env.NEXT_PUBLIC_WEBSITE_DOMAIN} wants you to sign in with your Ethereum account:\n${checksumAddress}\n\n\nURI: ${process.env.NEXT_PUBLIC_API_BASE_URL}\nVersion: 1\nChain ID: 1\nNonce: ${nonceData.nonce}\nIssued At: ${nonceData.issued_at}`;
+                // Sign message
+                signMessage(
+                  { message },
+                  {
+                    onSuccess(data, variables, context) {
+                      // Link wallet
+                      linkWallet(
+                        `${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/auth/eth/link`,
+                        checksumAddress,
+                        data
+                      )
+                        .then((res) => {
+                          if (res.ok) {
+                            res.json().then((data: { success: boolean }) => {
+                              setLinkSuccess(data.success);
+                              toast.success(
+                                `Wallet linked ${
+                                  data.success ? "successfully" : "failed"
+                                }!`
+                              );
+                            });
+                          } else {
+                            if (res.status === 403) {
+                              toast.error("Not Enough Funds!");
+                            } else {
+                              toast.error(`HTTP error! Status: ${res.status}`);
+                            }
+                          }
+                        })
+                        .finally(() => {
+                          disconnect();
+                          setIsRunning(false);
+                        });
+                    },
+                  }
+                );
+              });
+          } else {
+            if (response.status === 422) {
+              toast.error("Wallet Address is Incorrect");
+            } else {
+              toast.error(`HTTP error! Status: ${response.status}`);
+            }
+            console.error(`HTTP error! Status: ${response.status}`);
+            disconnect();
+            setIsRunning(false);
+          }
+        });
+      }
+    };
 
-  useEffect(() => {
-    if (nonceData && accountAddress) {
-      const checksumAddress = getAddress(accountAddress);
-      setSiweMessage(
-        `${process.env.NEXT_PUBLIC_WEBSITE_DOMAIN} wants you to sign in with your Ethereum account:\n${checksumAddress}\n\n\nURI: ${process.env.NEXT_PUBLIC_API_BASE_URL}\nVersion: 1\nChain ID: 1\nNonce: ${nonceData.nonce}\nIssued At: ${nonceData.issued_at}`
-      );
-    }
-  }, [nonceData, accountAddress]);
-
-  useEffect(() => {
-    if (siweMessage && !linkSuccess) {
-      signMessage({ message: siweMessage });
+    if (isConnected && accountAddress && !isRunning && !linkSuccess) {
+      connectWalletFlow();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [siweMessage, linkSuccess]);
-
-  useEffect(() => {
-    if (signMessageData && accountAddress && !linkSuccess) {
-      const checksumAddress = getAddress(accountAddress);
-      linkWallet(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/auth/eth/link`,
-        checksumAddress,
-        signMessageData
-      )
-        .then(() => {
-          setLinkSuccess(true);
-          toast.success("Wallet linked successfully!");
-          disconnect(); // Disconnect the wallet after successful link.
-        })
-        .catch((error) => {
-          toast.error(`Link wallet error: ${error.message}`);
-          console.error("Link wallet error:", error);
-        });
-    }
-  }, [signMessageData, accountAddress, disconnect, linkSuccess]);
-
-  useEffect(() => {
-    if (isConnected && accountAddress) {
-      fetchNonce();
-    }
-  }, [isConnected, accountAddress, fetchNonce]);
+  }, [isConnected, accountAddress]);
 
   useEffect(() => {
     if (signError) {
       toast.error("Sign message was cancelled.");
       disconnect(); // Disconnect the wallet on sign message cancellation
     }
-  }, [signError, disconnect]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [signError]);
 
   return (
     <ConnectKitButton.Custom>
